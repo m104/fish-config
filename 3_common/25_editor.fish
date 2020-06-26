27127e1b1fae5bc24c17c9f1d7647e110bbb1df3
Adam Collins <adam@m104.us>
2020-06-25 23:55:04 -0700

    Use nvim for confix function

--- 3_common/25_editor.fish
+++ 3_common/25_editor.fish
@@ -1,4 +1,12 @@
-debug "Setting EDITOR to vim"
-set -x EDITOR vim
-debug "Setting VISUAL to vim"
-set -x VISUAL vim
+if which -s nvim
+    debug "Detected nvim"
+    set -g vim_cmd nvim
+else
+    debug "No nvim detected; using vim"
+    set -g vim_cmd vim
+end
+
+debug "Setting EDITOR to $vim_cmd"
+set -x EDITOR $vim_cmd
+debug "Setting VISUAL to $vim_cmd"
+set -x VISUAL $vim_cmd
--- 3_common/30_nvim.fish
+++
@@ -1,5 +0,0 @@
-if which -s nvim
-    debug "Detected nvim"
-    set -g vim_cmd nvim
-end
-
